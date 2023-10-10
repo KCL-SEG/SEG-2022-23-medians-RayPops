c911f461e69123f9d10881853cfd21b5d88a42ab
Rayan Popat <rayan.popat@kcl.ac.uk>
2023-10-10 21:34:53 +0100

    Add Median Function
    Calculates the median when given a list of numbers.

--- median.py
+++ median.py
@@ -1,10 +1,19 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def median(numbers):
+    """Return the median of the given list of numbers."""
+    numbers.sort()
+    if len(numbers) % 2 == 0:
+        return (numbers[len(numbers) // 2 - 1] + numbers[len(numbers) // 2]) / 2
+    else:
+        return numbers[len(numbers) // 2]
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        print(median(numbers))
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
